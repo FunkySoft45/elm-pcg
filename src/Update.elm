9b0c43233d4f086cf4df33fb5f8a8facd2d2f226
Ace Anderson <funky.soft.45@gmail.com>
2016-06-21 17:20:10 +0200

    module Update with Steps

--- src/Update.elm
+++ src/Update.elm
@@ -1,3 +1,25 @@
 module Update exposing (..)
 
-        
+import Messages exposing (..)
+import Models exposing (..)
+import Result exposing (..)
+import String 
+
+import Steps exposing (..)
+    
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  
+  case msg of
+    Change newContent ->
+      ( { model | seed = Result.withDefault  0 (String.toInt newContent) }, Cmd.none )
+    Step1 ->
+       ( { model | content = (drawStep1 model.seed) }, Cmd.none )
+    Step2 ->
+       ( { model | content = (drawStep2 model.seed) }, Cmd.none )         
+    Step3 ->
+       ( { model | content = (drawStep3 model.seed) }, Cmd.none )         
+    Step4 ->
+       ( { model | content = (drawStep4 model.seed) }, Cmd.none )         
+    Step5 ->
+       ( { model | content = (drawStep5 model.seed) }, Cmd.none )        
